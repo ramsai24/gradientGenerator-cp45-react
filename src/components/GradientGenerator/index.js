4e5000c901ae4547adc812263d349edd4fb213d1
jnrsai24 <jnrsai24@gmail.com>
2023-08-30 03:58:02 +0000

    unable to visualize the render item due color of effect of their elements

--- src/components/GradientGenerator/index.js
+++ src/components/GradientGenerator/index.js
@@ -1,7 +1,33 @@
-const gradientDirectionsList = [
-  {directionId: 'TOP', value: 'top', displayText: 'Top'},
-  {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
-  {directionId: 'RIGHT', value: 'right', displayText: 'Right'},
-  {directionId: 'LEFT', value: 'left', displayText: 'Left'},
-]
+import {Component} from 'react'
+
+import {
+  Container,
+  Heading,
+  //   Paragraph,
+  //   ParagraphWhite,
+  //   Input,
+  //   Button,
+  //   GeneratorButton,
+} from './styledComponents'
+
+// const gradientDirectionsList = [
+//   {directionId: 'TOP', value: 'top', displayText: 'Top'},
+//   {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
+//   {directionId: 'RIGHT', value: 'right', displayText: 'Right'},
+//   {directionId: 'LEFT', value: 'left', displayText: 'Left'},
+// ]
 // Write your code here
+
+class GradientGenerator extends Component {
+  state: {top: '', bottom: '', left: '', right: ''}
+
+  render() {
+    return (
+      <Container>
+        <Heading>Generate a CSS Color Gradient</Heading>
+      </Container>
+    )
+  }
+}
+
+export default GradientGenerator
--- src/components/GradientGenerator/styledComponents.js
+++ src/components/GradientGenerator/styledComponents.js
@@ -1,1 +1,42 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  //   background-image: linear-gradient(
+  //     ${props => props.direction},
+  //     ${props => props.left},
+  //     ${props => props.right}}
+  //   );
+`
+
+export const Heading = styled.h1`
+  color: #ffffff79;
+`
+
+export const Paragraph = styled.p`
+  color: #ffffff79;
+`
+
+export const ParagraphWhite = styled(Paragraph)`
+  color: #ffffff;
+`
+
+export const Input = styled.input`
+  height: 40px;
+`
+
+export const Button = styled.button`
+  width: 120px;
+  height: 40px;
+  color: #1e293b;
+  background-color: ${props => (props.outline ? '#ededed' : '#ffffff79')};
+`
+
+export const GeneratorButton = styled(Button)`
+  background-color: #00c9b7;
+`
